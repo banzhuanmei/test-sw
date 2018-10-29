411b0c1d04bad4cd3a8a09b28e90672397e3e5db
litanda <lishunan0722@autohome.com.cn>
2018-10-29 18:10:08 +0800

    fix

--- service-worker.js
+++ service-worker.js
@@ -22,7 +22,7 @@ self.addEventListener('install', event => {
 	);
 });
 
-self.addEventListener('fetch', () => {
+self.addEventListener('fetch', event => {
 	event.respondWith(caches.match(event.request).then((response) => {
 		if (response !== undefined) {
 			return response;
