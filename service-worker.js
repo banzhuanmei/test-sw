e9c4fe129a908da9f817b7e6c749431bbad67fb2
litanda <lishunan0722@autohome.com.cn>
2018-11-21 16:17:32 +0800

    fix

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ const urlsToCache = [
 	'/test-sw/',
 	'/test-sw/css/style.css',
 	'/test-sw/images/banner.png',
-	// '/test-sw/data.js',
+	'/test-sw/data.js',
 	'/test-sw/error.html'
 ];
 // self 为当前 scope 内的上下文
