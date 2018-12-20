bcd7ec608931299ef8cacae6deaa221bbe94cf0d
litanda <lishunan0722@autohome.com.cn>
2018-12-20 10:21:32 +0800

    fix

---
+++ js/sw-wb.js
@@ -0,0 +1,38 @@
+importScripts ('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+if (workbox) {
+	console.log(`Yay! workbox is loaded 🎉`);
+}
+else {
+	console.log(`Boo! workbox didn't load 😬`);
+}
+workbox.setConfig({
+  debug: false
+});
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+var currentCacheNames = {
+  page: 'www:page',
+  static: 'www:static',
+  img: 'www:img'
+};
+
+// html、域名下资源缓存
+workbox.routing.registerRoute(
+  function({url, event}) {
+		console.log('$$$$$$$$$$' + url.hostname);
+    if (url.hostname === 'banzhuanmei.github.io') {
+      return (new RegExp('/(.*)').test(url.pathname))
+    } else {
+      return false;
+    }
+  },
+  workbox.strategies.networkFirst({
+    cacheName: currentCacheNames.page,
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 10
+      })
+    ]
+  })
+);
